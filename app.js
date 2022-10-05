b540eefb0fad786eb2118321e2733d517c0e194e
Emanuel Alamilla <e.alamilla318@gmail.com>
2022-10-05 00:40:22 -0500

    implementacion de base de datos de prueba de postgres junto con el modelo de usuario

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const sequelize = require('./config/db');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+try {
+  sequelize.authenticate();
+  sequelize.sync();
+  console.log('Database connected');
+} catch (error) {
+  console.log('Database not connected: ', error);
+}
 
 const listener = app.listen(process.env.PORT || 4000, () => {
     console.log(`Server is listening on port ` + listener.address().port);
